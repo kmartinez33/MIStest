82dacee00886b3025565828e6778db3b11d9757d
kmartinez33 <kmartinez3@babson.edu>
2016-09-06 14:58:09 -0400

    commit session2.py

--- session2.py
+++ session2.py
@@ -1,2 +1,37 @@
 print('Hello, world!')
-print('Hey JUde', 'dont make it bad')
+print('Hey Jude', "dont make it bad")
+print ('The total number of overall medals in Rio 2016 is', 46 + 37 + 38)
+message= 'Please enter your name: '
+print(message)
+name=input()
+print('Hello, ', name)
+message='I did somethign cool today!'
+n=100
+print(n+100)
+a=123
+print(a)
+a='ABC'
+print(a)
+a='ABC'
+b=a
+a='XYZ'
+print(b)
+x=10
+x=x+2
+print(x)
+#'2' - '1'
+#'EU' - 'Great Britain'
+first_name='John'
+last_name='Lennon'
+print(first_name+last_name)
+print('Naah, na na nanana naah, nanana naah, hey Jude. ' * 10)
+print('Hello, %s' % 'world')
+print('Congratulations, %s, you won %dth Academy Award.' % ('Leonardo', 88))
+print('Today is %2d-%06d.' % (9,6)) #the number in front of the d is the number of spaces before it shows the number and the zero is front of the number will put as many zeros as that number.
+print('Pi equals %.2f.' % 3.1415926) # f=floating like fractions or decimals
+print('Age: %s. Gender:%s' % (20, True)) #s= string and is used for names etc
+print('Growth rate: %d %%' % 7)
+minute=45 #CURRENT time
+percentage=(minute*100) / 60
+print(percentage)
+print(11 % 5) # this will only give you the remainder of the division.
