c142d293a6073c7b9b01951b7a87c97f1294f869
kmartinez33 <kmartinez3@babson.edu>
2016-09-13 13:47:45 -0400

    commit session4.py

---
+++ session4.py
@@ -0,0 +1,20 @@
+
+type(42)
+type('42')
+int('42')
+int('Hello') #will not go through bc not integer
+int(3.99)
+int(-2.3)
+float(42)
+float('3.14')
+abs(-939)
+abs(-100,42)
+max(1,3) # gives maximum out of numbers
+round(3.88789)
+min(4,-89,10)
+ord(7,10,-866,-837)
+chr(ord('A')+32)
+ord('a') - ord('A')
+ord('b') - ord('B')
+
+input()
