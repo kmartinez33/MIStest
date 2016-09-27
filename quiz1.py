2118674b1d78577bcec6685c4e54cde115a204e8
kmartinez33 <kmartinez3@babson.edu>
2016-09-27 13:37:01 -0400

    commit quiz.py

---
+++ quiz1.py
@@ -0,0 +1,34 @@
+####Question 1#####
+def crazy_about_9(a, b):
+    if b == 0:
+       return a
+    else:
+        return gcd(b, a % b)  
+
+print(crazy_about_9(2, 9))
+print(crazy_about_9(4, 5))
+print(crazy_about_9(3, 8))
+
+####Question 2####
+
+def leap_year(year):
+
+    year(int): a year
+    
+print(leap_year(1900))
+print(leap_year(2016))
+print(leap_year(2017))
+print(leap_year(2000))
+
+Question 3:
+def sum_squares(n):
+    pass
+while True:
+    print(n)
+    y =(n+a/n)/2
+    if y==x:
+        break
+    n=y
+print(sum_squares(1))
+print(sum_squares(100))
+input()
